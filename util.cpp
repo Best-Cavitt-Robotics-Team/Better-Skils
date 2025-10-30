d96e29b5dd1e763c0d32bb43f740c6dd0a86ef71
Best-Cavitt-Robotics-Team <bestcavittvexteam@gmail.com>
2025-10-29 19:49:54 -0700

    Add files via upload

--- util.cpp
+++ util.cpp
@@ -1,4 +1,7 @@
 #include "vex.h"
+#include "cmath"
+#include <iostream>
+#include <algorithm> 
 
 /**
  * Converts an angle to an equivalent one in the range [0, 360).
@@ -137,7 +140,7 @@ int to_port(int port){
  */
 
 float deadband(float input, float width){
-  if (std::fabs(input)<width){
+  if (fabs(input)<width){
     return(0);
   }
   return(input);
@@ -172,7 +175,7 @@ bool is_line_settled(float desired_X, float desired_Y, float desired_angle_deg, 
  */
 
 float left_voltage_scaling(float drive_output, float heading_output){
-  float ratio = std::max(std::fabs(drive_output+heading_output), std::fabs(drive_output-heading_output))/12.0;
+  float ratio = std::max(fabs(drive_output+heading_output), fabs(drive_output-heading_output))/12.0;
   if (ratio > 1) {
     return (drive_output+heading_output)/ratio;
   }
@@ -190,7 +193,7 @@ float left_voltage_scaling(float drive_output, float heading_output){
  */
 
 float right_voltage_scaling(float drive_output, float heading_output){
-  float ratio = std::max(std::fabs(drive_output+heading_output), std::fabs(drive_output-heading_output))/12.0;
+  float ratio = std::max(fabs(drive_output+heading_output), fabs(drive_output-heading_output))/12.0;
   if (ratio > 1) {
     return (drive_output-heading_output)/ratio;
   }
